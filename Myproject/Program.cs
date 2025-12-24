f5a025cabbf656934cdee381020d4fa1767fc171
Jake Zyril Azores <jakeazores99@gmail.com>
2025-12-24 05:05:31 +0000

    Task3 commit for message submission

---
+++ Myproject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Task3
+{
+    static void Main()
+    {
+       
+        int[] numbers = { 3, 7, 12, 19, 21, 25, 30 };
+
+        
+        Console.Write("Enter a number to search for: ");
+        int searchNumber;
+        
+        
+        if(int.TryParse(Console.ReadLine(), out searchNumber))
+        {
+            bool found = false; 
+            
+           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                 
+                if (numbers[i] == searchNumber)
+                {
+                    Console.WriteLine("Number found at position " + i + "!");
+                    found = true;
+                    break;  
+                }
+            }
+
+            
+            if (!found)
+            {
+                Console.WriteLine("Number not found in the list.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
